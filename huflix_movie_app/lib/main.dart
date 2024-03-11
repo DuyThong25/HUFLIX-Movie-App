c4876f78776cc372588e732d0c55aa7409df2d48
Nguyễn Anh Thành <anhthanh260599@gmail.com>
2024-03-11 21:53:08 +0700

    lấy api

--- huflix_movie_app/lib/main.dart
+++ huflix_movie_app/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:huflix_movie_app/api/api_clients.dart';
+import 'package:huflix_movie_app/api/data_sources/movie_remote_data_source.dart';
 import 'package:huflix_movie_app/views/home/home_page.dart';
-
+import 'package:http/http.dart' as http;
 void main() {
+  ApiClient apiClient = ApiClient(http.Client());
+  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
+  dataSource.getTrending();
+  dataSource.getPopular();
+  dataSource.getPlayingNow();
+  dataSource.getComingSoon();
   runApp(const MainApp());
 }
 
