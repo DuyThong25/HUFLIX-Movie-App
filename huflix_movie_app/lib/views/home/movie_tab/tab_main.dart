059603350fc75317db1135781eb2def7253701f7
Adtnt <125952366+Adtnt@users.noreply.github.com>
2024-03-12 00:11:53 +0700

    Update_movie_tab

--- huflix_movie_app/lib/views/home/movie_tab/tab_main.dart
+++ huflix_movie_app/lib/views/home/movie_tab/tab_main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:huflix_movie_app/views/home/movie_tab/tab_title.dart';
 import 'package:huflix_movie_app/views/home/movie_tab/tab_view.dart';
 
 class TabMain extends StatelessWidget {
@@ -7,14 +6,35 @@ class TabMain extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const DefaultTabController(
+    return DefaultTabController(
       length: 3,
       child: Column(
         children: <Widget>[
           // Title of Tab
-          TabTitle(),
+          Container(
+      color: Colors.black,
+      child: const TabBar(
+        // Custom tab tilte
+        dividerColor: Colors.transparent,
+        indicatorColor: Colors.purple,
+        labelColor: Colors.purple,
+        indicatorSize: TabBarIndicatorSize.label,
+        unselectedLabelColor: Colors.grey,
+        unselectedLabelStyle:
+            TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        labelStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        padding: EdgeInsets.only(left: 30, right: 30),
+        indicatorWeight: 4,
+        // Data title
+        tabs: [
+          Tab(text: "Phổ biến"),
+          Tab(text: "Đang chiếu"),
+          Tab(text: "Sắp chiếu"),
+        ],
+      ),
+    ),
           // Tab view data
-          MyTabView(),
+          const MyTabView(),
         ],
       ),
     );
